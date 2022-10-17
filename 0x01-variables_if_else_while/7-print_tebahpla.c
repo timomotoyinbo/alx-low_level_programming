00466bac1e10cae774e1d86631c5fc97518449fb
timomotoyinbo <timomotoyinbo@gmail.com>
2022-10-17 11:24:58 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - print the alphabets in reverse
+* Return: Always 0
+*/
+
+int main(void)
+{
+	char le;
+
+	for (le = 'z'; let >= 'a'; le--)
+		putchar(le);
+
+	putchar('\n');
+
+	return (0);
+}
