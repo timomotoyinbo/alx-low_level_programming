8346609aee79a99596a110171b4a5389cf5cf6b4
timomotoyinbo <timomotoyinbo@gmail.com>
2022-10-19 15:13:37 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * _abs - computes the abs value of an int
+  * @n: check n for absolute value
+  * Return: the abs of the int.
+  */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
