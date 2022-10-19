da6cd3085c525e47ea98760e21bb06304bab3ef7
timomotoyinbo <timomotoyinbo@gmail.com>
2022-10-19 13:53:01 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include  "main.h"
+
+/**
+  * print_alphabet_x10 - print 10 times the alphabets, in lowercase
+  */
+
+void print_alphabet_x10(void)
+{
+	int k;
+	char key;
+
+	for (k = 1; k < 10; k++)
+	{
+		for (key = 'a'; key <= 'z'; key++)
+		{
+			_putchar(key);
+		}
+		_putchar(key);
+		k;
+	}
+}
