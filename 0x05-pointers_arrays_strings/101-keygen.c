fd4a661fb99bb0e25b3cba89e11d7304eaf1eb14
timomotoyinbo <timomotoyinbo@gmail.com>
2022-10-25 16:27:15 -0700

    Task 101

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,18 +9,44 @@
 
 int main(void)
 {
-	int pass, sum;
+	char password[84];
+	int index = 0, sum = 0, diff_half1, diff_half2;
 
-	srand(time(NULL));
-	sum = 0; 
+	srand(time(0));
 
-	while (sum <= 2645)
+	while (sum < 2772)
 	{
-		pass = (rand() % 128);
-		sum += pass;
-		printf("%c", pass);
+		password[index] = 33 + rand() % 94;
+		sum += password[index];
 	}
-	printf("%c", 22);
+
+	password[index] = '\0';
+
+	if (sum != 2772)
+	{
+		diff_half1 = (sum - 2772) / 2;
+		diff_half2 = (sum - 2772) / 2;
+		if ((sum - 2772) % 2 != 0)
+			diff_half++;
+
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half1))
+			{
+				password[index] -= diff_half1;
+				break;
+			}
+		}
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half2))
+			{
+				password[index] -= diff_half2;
+				break;
+			}
+		}
+	}
+	print("%s", password);
 
 	return (0);
 }
