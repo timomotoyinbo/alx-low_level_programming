5a7704ae4450c2e4df62be0121223b6eb585c8dc
timomotoyinbo <timomotoyinbo@gmail.com>
2022-11-09 16:53:29 -0800

    Task 3b

--- 0x0B-malloc_free/3-main.c
+++ 0x0B-malloc_free/3-main.c
@@ -14,22 +14,21 @@
 void print_grid(int **grid, int width, int height)
 {
 
-    int w;
-    int h;
+	int w;
+	int h;
 
-    h = 0;
-
-    while (h < height)
-    {
-        w = 0;
-        while (w < width)
-        {
-            printf("%d ", grid[h][w]);
-            w++;
-        }
-        printf("\n");
-        h++;
-    }   
+	h = 0;
+	while (h < height)
+	{
+		w = 0;
+		while (w < width)
+		{
+			printf("%d ", grid[h][w]);
+			w++;
+		}
+		printf("\n");
+		h++;
+	}
 }
 
 /**
@@ -40,17 +39,17 @@ void print_grid(int **grid, int width, int height)
 
 int main(void)
 {
-    int **grid;
+	int **grid;
 
-    grid = alloc_grid(48, 32);
-    if (grid == NULL)
-    {
-        return (1);
-    }
-    print_grid(grid, 48, 32);
-    printf("\n");
-    grid[0][3] = 98;
-    grid[3][4] = 402;
-    print_grid(grid, 6, 4);
-    return (0);
+	grid = alloc_grid(48, 32);
+	if (grid == NULL)
+	{
+		return (1);
+	}
+	print_grid(grid, 48, 32);
+	printf("\n");
+	grid[0][3] = 98;
+	grid[3][4] = 402;
+	print_grid(grid, 6, 4);
+	return (0);
 }
