d0f786c017994babb606ff4cb28c29d34a4f5bf5
timomotoyinbo <timomotoyinbo@gmail.com>
2022-11-09 17:04:32 -0800

    Task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -29,15 +29,12 @@ int **alloc_grid(int width, int height)
 				free(array[i]);
 			free(array);
 			return (NULL);
-
 		}
 	}
-
-
 
 	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
 			array[i][j] = 0;
 
-			return (array);
+	return (array);
 }
